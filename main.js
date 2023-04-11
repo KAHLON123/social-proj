90f8fbd7a10eda5580a755e3b0b093519c4d5aaf
KAHLON123 <112655375+KAHLON123@users.noreply.github.com>
2023-04-11 11:36:19 -0600

    added

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 let preBtn = document.getElementById('pre-power');
 let powerBtn = document.getElementById('power');
 let postBtn = document.getElementById('post-power');
+let bibBtn = document.getElementById('biblio-btn');
 
 //div
 const preDiv = document.getElementById("pre-txt");
 const powerDiv = document.getElementById("power-txt");
 const postDiv = document.getElementById("post-txt");
+const bibDiv = document.getElementById("biblio");
 
 preBtn.addEventListener('click', preHandler);
 function preHandler(){
@@ -19,4 +21,8 @@ function powerHandler(){
 postBtn.addEventListener('click', postHandler);
 function postHandler(){
     postDiv.classList.toggle('hidden');
+}
+bibBtn.addEventListener('click', biblioHandler);
+function biblioHandler(){
+    bibDiv.classList.toggle('hidden');
 }
