9dc6577a0e42fc93bef1ff3f2d32906fbfcc90d1
KAHLON123 <112655375+KAHLON123@users.noreply.github.com>
2023-04-11 00:32:57 -0600

    lots added. research left only

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-let yearBtn = document.getElementById('year-btn');
-let yearOut = document.getElementById('year-span');
-let out = '';
+//btn
+let preBtn = document.getElementById('pre-power');
+let powerBtn = document.getElementById('power');
+let postBtn = document.getElementById('post-power');
 
-yearBtn.addEventListener('click', yearHandler);
-function yearHandler(){
-    let s = document.getElementById('dropdown');
-    switch (s){
-        case "pre-power":
-            out = "timeline/pre.txt";
-            break;
-        case "power":
-            out = "timeline/power.txt";
-            break;
-        case "post-power":
-            out = "timeline/post.txt";
-            break;
-    }
-    yearOut.innerHTML = out;
+//div
+const preDiv = document.getElementById("pre-txt");
+const powerDiv = document.getElementById("power-txt");
+const postDiv = document.getElementById("post-txt");
+
+preBtn.addEventListener('click', preHandler);
+function preHandler(){
+    preDiv.classList.toggle('hidden');
+}
+powerBtn.addEventListener('click', powerHandler);
+function powerHandler(){
+    powerDiv.classList.toggle('hidden');
+}
+postBtn.addEventListener('click', postHandler);
+function postHandler(){
+    postDiv.classList.toggle('hidden');
 }
